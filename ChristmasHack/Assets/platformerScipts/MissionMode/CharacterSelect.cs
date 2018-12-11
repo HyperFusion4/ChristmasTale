b9633b303ec505806d404e1a794c8767daf5c119
Colby Johnson <20.colby.johnson@ksd.org>
2018-12-11 10:28:54 -0800

    2player

---
+++ ChristmasHack/Assets/platformerScipts/MissionMode/CharacterSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour {
+    public Text Character;
+    public int cac = 0;
+    public void Hero()
+    {
+        Character.GetComponent<Text>().text = "Hero";
+    }
+
+    public void Riba()
+    {
+        Character.GetComponent<Text>().text = "Riba";
+    }
+
+    public void Frost()
+    {
+        Character.GetComponent<Text>().text = "Frost";
+    }
+
+    public void Villain()
+    {
+        Character.GetComponent<Text>().text = "Villain";
+    }
+
+}
